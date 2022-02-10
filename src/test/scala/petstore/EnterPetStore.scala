7e3599373926193b74ebe54361d0aaaaa4a937ae
Simran Pal <simranpalmeerut@gmail.com>
2022-02-10 11:33:58 +0530

    Create petstore simulation from recorded file

---
+++ src/test/scala/petstore/EnterPetStore.scala
@@ -0,0 +1,36 @@
+package petstore
+
+import scala.concurrent.duration._
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.jdbc.Predef._
+
+class EnterPetStore extends Simulation {
+
+	val httpProtocol = http
+		.baseUrl("https://petstore.octoperf.com")
+		.acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
+		.acceptEncodingHeader("gzip, deflate")
+		.acceptLanguageHeader("en-GB,en-US;q=0.9,en;q=0.8")
+		.upgradeInsecureRequestsHeader("1")
+		.userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36")
+
+	val headers_0 = Map(
+		"sec-ch-ua" -> """ Not A;Brand";v="99", "Chromium";v="98", "Google Chrome";v="98""",
+		"sec-ch-ua-mobile" -> "?0",
+		"sec-ch-ua-platform" -> "macOS",
+		"sec-fetch-dest" -> "document",
+		"sec-fetch-mode" -> "navigate",
+		"sec-fetch-site" -> "same-origin",
+		"sec-fetch-user" -> "?1")
+
+
+
+	val scn = scenario("EnterPetStore")
+		.exec(http("request_0")
+			.get("/actions/Catalog.action")
+			.headers(headers_0))
+
+	setUp(scn.inject(atOnceUsers(1))).protocols(httpProtocol)
+}
